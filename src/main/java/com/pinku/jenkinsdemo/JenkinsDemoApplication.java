528a78335523efe7a0d7d24f0ca1b6101963f955
pinku <kpinku32@gmail.com>
2020-09-27 00:34:44 +0530

    First commit about jenkins

---
+++ src/main/java/com/pinku/jenkinsdemo/JenkinsDemoApplication.java
@@ -0,0 +1,20 @@
+package com.pinku.jenkinsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class JenkinsDemoApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsDemoApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application)
+	{
+		return application.sources(JenkinsDemoApplication.class);
+	}
+}
---
+++ src/main/java/com/pinku/jenkinsdemo/controller/JenkinsTestController.java
@@ -0,0 +1,13 @@
+package com.pinku.jenkinsdemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JenkinsTestController {
+
+	@GetMapping("/show")
+	public String showMessage() {
+		return "Coming from show message";
+	}
+}
